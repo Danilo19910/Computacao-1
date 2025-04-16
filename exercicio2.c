e5b8b05beea4df8c33d5e32a91d610b60de43f24
Danilo19910 <socher@alunos.utfpr.edu.br>
2025-04-16 10:09:25 -0300

    Update exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int n = 0, x = 0, i = 0;
-    float c = 0;
+    int n = 0, x = 0, p = 0;
+    float m = 0;
 
     printf("Digite um número: ");
     scanf("%d", &n);
@@ -13,14 +13,14 @@ int main()
         if (n>0)
 
         {
-            i++;
+            p++;
             x = x + n;
         }
         printf("Digite um número: ");
         scanf("%d", &n);
     }
-    printf("Quantidade de números positivos: %d\n", i);
-    printf("A média dos valores positivos é: %.1f", c=x/i);
+    printf("Quantidade de números positivos: %d\n", p);
+    printf("A média dos valores positivos é: %.1f", m=x/p);
 
     return 0;
 
