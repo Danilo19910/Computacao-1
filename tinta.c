0f3a4a136aebe697fb691b0e461d02ebdb8d4ef1
Danilo19910 <socher@alunos.utfpr.edu.br>
2025-04-23 08:58:35 -0300

    Add files via upload

---
+++ tinta.c
@@ -0,0 +1,35 @@
+#include<math.h>
+#include<stdio.h>
+
+int main()
+{
+    float Altura, Largura;
+
+    printf("Qual a altura em metros? ");
+    scanf("%f", &Altura);
+    printf("Qual a largura em metros? ");
+    scanf("%f", &Largura);
+
+
+    if (Altura <= 0 || Largura <= 0)
+    {
+        printf("Erro, digite valores positivos.");
+        return 1;
+    }
+
+    float Area = Altura * Largura;
+    float Tinta = Area / 2;
+    Tinta = ceil(Tinta);
+
+    if (Area == 0)
+    {
+        printf("Não há área para pintar.");
+    }
+    else
+    {
+        printf("Área da parede: %0.1f\n", Area);
+        printf("Quantidade de tinta necessária: %0.1f\n", Tinta);
+    }
+
+    return 0;
+}
